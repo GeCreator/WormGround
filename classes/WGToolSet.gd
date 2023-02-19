e82c913d19d45fe64a55681f81898a5f7e04fcb5
gecreator <ge-rom@mail.ru>
2023-02-19 23:16:59 +0300

    set toolset to save data in hidden fileds

--- classes/WGToolSet.gd
+++ classes/WGToolSet.gd
@@ -1,9 +1,42 @@
 @tool
 class_name WGToolSet extends Resource
 
-@export var _surfaces: Dictionary
+var _surfaces: Dictionary
+var _borders: Dictionary
+var _decals: Dictionary
+var _brushes: Dictionary
 
-func add_surface(surface_name: String, surface: WGSurface) -> bool:
-    if _surfaces.has(surface_name): return false
-    _surfaces[surface_name] = surface
-    return true
+func make_surface() -> WGSurface:
+    var surface := WGSurface.new()
+    var insert_id: int = 1
+    for k in _surfaces:
+        print(int(k))
+        if insert_id<=int(k):
+            insert_id = int(k)+1
+    _surfaces[insert_id] = surface
+    return surface
+
+func _get_property_list():
+    return [
+        {
+        "name": "_borders",
+        "type": TYPE_DICTIONARY,
+        "usage": PROPERTY_USAGE_STORAGE,
+        },
+        {
+        "name": "_surfaces",
+        "type": TYPE_DICTIONARY,
+        "usage": PROPERTY_USAGE_STORAGE,
+        },
+        {
+        "name": "_decals",
+        "type": TYPE_DICTIONARY,
+        "usage": PROPERTY_USAGE_STORAGE,
+        },
+        {
+        "name": "_brushes",
+        "type": TYPE_DICTIONARY,
+        "usage": PROPERTY_USAGE_STORAGE,
+        }
+    ]
+
