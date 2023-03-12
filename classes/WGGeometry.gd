661192c5cee69cd7633bf28dc57dd05e3b9e7711
gecreator <ge-rom@mail.ru>
2023-03-12 23:18:06 +0300

    fix uncorrect left right point detection

--- classes/WGGeometry.gd
+++ classes/WGGeometry.gd
@@ -392,10 +392,12 @@ func _get_left_right_vertexes(shape: PackedVector2Array) -> PackedInt32Array:
     var left: float = INF
     var right: float = -INF
     for p in shape:
-        if p.x<left:
-            left = p.x
-            result[0] = i
-        if p.x>right:
+        var s = _get_segment(i, shape)
+        if s[0].y<s[1].y:
+            if p.x<left:
+                left = p.x
+                result[0] = i
+        elif p.x>right:
             right = p.x
             result[1] = i
         i+=1
