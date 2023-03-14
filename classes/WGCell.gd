145f875a731093177a49ac0980c072f73a79278e
gecreator <ge-rom@mail.ru>
2023-03-14 19:18:15 +0300

    cleanup

--- classes/WGCell.gd
+++ classes/WGCell.gd
@@ -49,36 +49,3 @@ func remove(shape: PackedVector2Array):
         polygons = _geometry.remove(shape, polygons)
         _surfaces[sid] = polygons #_make_optimized(polygons)
     emit_signal('changed')
-
-func _make_optimized(polygons: Array) -> Array:
-    var result : = Array()
-    for p in polygons:
-        var optimized = _optimise(p)
-        if optimized.size()>2:
-            result.append(optimized)
-    return result
-           
-func _optimise(shape: PackedVector2Array) -> PackedVector2Array:
-    var result := PackedVector2Array()
-    result.append(shape[-2])
-    var previous: Vector2 = shape[-2]    
-    for i in range(-1,shape.size()-2):        
-        var current: Vector2  = shape[i]
-        if _vertex_on_border(current):
-            result.append(current)
-            previous = current
-        else:
-            var next = shape[i+1]
-            var a = previous-current
-            var b = next-current
-            var angle = absf(rad_to_deg(a.angle_to(b)))
-            # обрезаем острые углы
-            if (angle>30.0):
-                result.append(current)
-            previous = current
-    return result
-
-func _vertex_on_border(v: Vector2) -> bool:
-    var mod = v.abs().posmod(_size)
-    var b = 1.0
-    return (mod.x<b or mod.x>(_size-b)) or (mod.y<b or mod.y>(_size-b))
