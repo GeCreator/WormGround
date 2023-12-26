c8ed268a6d2823d26094ac86531afb1741af77d1
gecreator <ge-rom@mail.ru>
2023-12-26 14:04:24 +0400

    remove lines on border

--- classes/WGBorder.gd
+++ classes/WGBorder.gd
@@ -5,7 +5,47 @@ static func render(canvas: Array, cell: Array, thickness: float, color: Color) -
     var canvas_rid = canvas[WGCanvas.DATA_CANVAS]
     var shapes: Array[PackedVector2Array] = cell[WGCell.DATA_SURFACE]
     for shape in shapes:
-        RenderingServer.canvas_item_add_polyline(canvas_rid, shape, PackedColorArray([color]), thickness)
+        var parts = _get_parts(shape.size(), _get_bad_segments(shape))
+        for line in _make_polylines(shape, parts):
+            RenderingServer.canvas_item_add_polyline(canvas_rid, line, PackedColorArray([color]), thickness)
+
+static func _make_polylines(shape: PackedVector2Array, parts: Array) -> Array[PackedVector2Array]:
+    
+    var result :Array[PackedVector2Array]
+    for part in parts:
+        var s: PackedVector2Array = []
+        for n in part: s.append(shape[n])
+        result.append(s)
+    return result
+
+static func _get_bad_segments(shape: PackedVector2Array) -> PackedInt32Array:
+    var result: PackedInt32Array = []
+    var size: int = shape.size()
+    for n in size:
+        if _segment_on_border(_get_segment(n, shape, size)):
+            result.append(n)
+    return result
+
+static func _segment_on_border(segment: PackedVector2Array) -> bool:
+    var a: Vector2 = segment[0].posmod(WormGround.CELL_SIZE)
+    var b: Vector2 = segment[1].posmod(WormGround.CELL_SIZE)
+    
+    if int(a.x)==int(b.x):
+        return int(a.x)==0
+    if int(a.y)==int(b.y):
+        return int(a.y)==0
+    return false
+
+
+## return PackedVector2Array with 2 points of segment n
+static func _get_segment(n: int, polygon: PackedVector2Array, size: int) -> PackedVector2Array:
+    return PackedVector2Array([
+        polygon[wrapi(n,0, size)],
+        polygon[wrapi(n+1,0, size)]
+    ])
+    
+static func _segment_is_on_border(segment: PackedVector2Array) -> bool:
+    return true
 
 static func _get_parts(shape_size: int, bad_segments: Array) -> Array:
     var indexes = range(0,shape_size)
