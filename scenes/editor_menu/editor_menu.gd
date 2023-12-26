1752b770e1cefa3c08038b9c79381ca4714bafa2
gecreator <ge-rom@mail.ru>
2023-12-26 12:09:59 +0400

    change icon

--- scenes/editor_menu/editor_menu.gd
+++ scenes/editor_menu/editor_menu.gd
@@ -3,7 +3,7 @@ extends HBoxContainer
 signal action(name, value)
 
 func _ready():
-    $edit.icon = get_theme_icon("CanvasItem", "EditorIcons")
+    $edit.icon = get_theme_icon("CanvasLayer", "EditorIcons")
 
 func _on_edit_pressed():
     emit_signal("action", "tool_brush", null)
